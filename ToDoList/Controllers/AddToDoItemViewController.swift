37681bde6c099cc1c5e24156468088326353816c
Rick Mak <rick.mak@gmail.com>
2018-05-04 14:54:48 +0800

    Remove commented code

--- ToDoList/Controllers/AddToDoItemViewController.swift
+++ ToDoList/Controllers/AddToDoItemViewController.swift
@@ -13,28 +13,6 @@ class AddToDoItemViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var saveButton: UIBarButtonItem!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
--- ToDoList/Controllers/FinishedTableViewController.swift
+++ ToDoList/Controllers/FinishedTableViewController.swift
@@ -10,21 +10,6 @@ import UIKit
 
 class FinishedTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -65,30 +50,4 @@ class FinishedTableViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .right)
         }
     }
-
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
